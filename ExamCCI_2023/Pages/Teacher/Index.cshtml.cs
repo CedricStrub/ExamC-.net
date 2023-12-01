1b90945bf553e069ec21cfff5ab8558bcafd84ee
CedricStrub <strubcedric@gmail.com>
2023-12-01 13:32:11 +0100

    1.1 le projet démare

---
+++ ExamCCI_2023/Pages/Teacher/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace ExamCCI_2023.Pages.Teacher
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Model.ApplicationDbContext _context;
+
+        public IndexModel(Model.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Model.Teacher> Teacher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Teachers != null)
+            {
+                Teacher = await _context.Teachers
+                                .ToListAsync();
+
+            }
+        }
+
+    }
+}
